531ddd8a7324e3ede225aa1fb2250a711c1e2846
MBIng92 <72155067+MBIng92@users.noreply.github.com>
2021-01-29 17:23:10 +0100

    Delete Tetris directory

--- Tetris/src/SortReverse.java
+++
@@ -1,8 +0,0 @@
-import java.util.*; //for sort algorithm
-
-//Sort algorithm (from biggest to smallest number)
-public class SortReverse implements Comparator<Score> {
-	public int compare(Score a, Score b) {
-		return b.getScore() - a.getScore();
-	}
-}
